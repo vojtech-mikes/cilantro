46649b84cf82622af5192e9303f49c5d0224a2e5
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-05-22 20:33:10 +0200

    Added FE and DB logic

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,20 @@
-import os
-
-from flask import Flask
-
+from flask import Flask, render_template, request, make_response
+from flaskr import database
+import feedparser
 
 def create_app():
     app = Flask(__name__)
 
+    database.table_init_check() 
+
     @app.route("/")
     def home():
-        return "Hello World"
+        feeds = database.load_saved_urls()
+        return render_template('base.html', feeds=feeds)
+
+    @app.route("/loadrss", methods=["POST"])
+    def load_rss():
+        print(request.form)
+        return "OK", 200
 
     return app
---
+++ flaskr/database.py
@@ -0,0 +1,64 @@
+import os
+import sqlite3
+from typing import List, Tuple
+
+
+def _get_db_cursor() -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    os.makedirs("db", exist_ok=True)
+    sqlite_cnx = sqlite3.connect("db/cilantro.db")
+
+    return sqlite_cnx, sqlite_cnx.cursor()
+
+
+def table_init_check() -> None:
+    conn, cursor = _get_db_cursor() 
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS feeds (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT UNIQUE NOT NULL
+    )
+    """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def load_saved_urls() -> List[str]:
+    conn, cursor = _get_db_cursor()
+
+    cursor.execute("""
+    SELECT url FROM feeds
+    """)
+
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return [x[0] for x in result]
+
+
+def create_record(url: str) -> None:
+    conn, cursor = _get_db_cursor()
+
+    cursor.execute("""
+    INSERT INTO feeds (url) VALUES (?)
+    """, (url,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def remove_record(url: str) -> None:
+    conn, cursor = _get_db_cursor()
+
+    cursor.execute("""
+    DELETE FROM feeds WHERE url=(?)
+    """, (url,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
